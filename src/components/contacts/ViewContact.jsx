806286a9ba2df28154b4955e43aa6cee315e5656
sreekanth <sreekanth>
2024-09-28 08:49:20 +0530

    updated list

--- src/components/contacts/ViewContact.jsx
+++ src/components/contacts/ViewContact.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ContactServer } from '../servers/ContactServer';
 import Spinner from './Spinner';
+import './viewcontact.css'; // Import the CSS file
 
 const ViewContact = () => {
   const { contactId } = useParams();
@@ -12,31 +13,26 @@ const ViewContact = () => {
     errorMessage: ""
   });
 
-  
-useEffect(()=>{
-
-  const fetchData = async ()=>{
-     try {
-      setState({...state, loading:true});
-      let response = await ContactServer.getOne(contactId)
-       setState({
-        ...state,
-        loading:false,
-        contact:response.data
-       })
-     } catch (error) {
-      setState({
-        ...state,
-        loading:false,
-        errorMessage:error.message
-       })
-     }
-  }
-  fetchData()
-
-},[contactId])
-
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setState({ ...state, loading: true });
+        let response = await ContactServer.getOne(contactId);
+        setState({
+          ...state,
+          loading: false,
+          contact: response.data
+        });
+      } catch (error) {
+        setState({
+          ...state,
+          loading: false,
+          errorMessage: error.message
+        });
+      }
+    };
+    fetchData();
+  }, [contactId]);
 
   let { contact, loading, errorMessage } = state;
 
@@ -44,16 +40,18 @@ useEffect(()=>{
     <>
       {loading ? (
         <Spinner />
+      ) : errorMessage ? (
+        <div className="error">{errorMessage}</div>
       ) : (
         contact && (
-          <div className="card" style={{ width: '18rem' }}>
+          <div className="card">
             <div className="card-body">
-              <p>Name: {contact.name}</p>
-              <p>Age: {contact.age}</p>
-              <p>Blood Group: {contact.blood}</p>
-              <p>Phone Number: {contact.phone}</p>
-              <p>Gender: {contact.gender}</p>
-              <p>Place: {contact.place}</p>
+              <p><strong>Name:</strong> {contact.name}</p>
+              <p><strong>Age:</strong> {contact.age}</p>
+              <p><strong>Blood Group:</strong> {contact.blood}</p>
+              <p><strong>Phone Number:</strong> {contact.phone}</p>
+              <p><strong>Gender:</strong> {contact.gender}</p>
+              <p><strong>Place:</strong> {contact.place}</p>
             </div>
           </div>
         )
